1bd68b98395684f29281f21cb07ddd8e72126851
Héctor Jorge Morales Arch <hjma98@gmail.com>
2023-04-25 11:48:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import request
 import os
 import json
 import pandas as pd
+
+def JSON2SCV(data):
+    # Definir la ruta y nombre de archivo
+    filename = 'archivo.csv'
+    # Comprobar si el archivo CSV ya existe
+    if not os.path.isfile(filename):
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            # Escribir los encabezados
+            writer.writerow(['directory', 'hash', 'height', 'id', 'image', 'change', 'owner', 'parent_id', 'rating', 'sample', 'sample_height', 'sample_width', 'score', 'tags', 'width'])
+    
