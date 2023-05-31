e30c3cf12581762c0ffc8db77c8d3243056a8165
Héctor Jorge Morales Arch <hjma98@gmail.com>
2023-05-31 08:55:20 -0600

    Characters

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import scv
-import request
+import requests
 import os
 import json
-import pandas as pd
+import csv
 
+
+# Lista de personajes y sus respectivas etiquetas
+characters = {
+    'Hakurei Reimu': 'reimu_hakurei',
+    'Kirisame Marisa': 'kirisame_marisa',
+    'Cirno': 'cirno',
+    'Komeiji Koishi': 'komeiji_koishi',
+    'Komeiji Satori': 'komeiji_satori',
+    'Yakumo Yukari': 'yakumo_yukari',
+    'Shameimaru Aya': 'shameimaru_aya',
+    'Fujiwara No Mokou': 'fujiwara_no_mokou',
+    'Houraisan Kaguya': 'houraisan_kaguya',
+    'Rumia': 'rumia'
+}
