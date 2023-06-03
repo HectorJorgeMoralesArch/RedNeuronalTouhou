1a394a06b7e3f8c4e60c84b2246221e9f6d47723
Héctor Jorge Morales Arch <hjma98@gmail.com>
2023-06-03 15:05:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import csv
 import random
 import shutil
+import concurrent.futures
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix, classification_report
@@ -16,7 +17,7 @@ def JSON2SCV(data, Character, folder):
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
             # Escribir los encabezados
-            writer.writerow(['id', 'image', 'tags', 'width', 'height', 'character', 'Path'])
+            writer.writerow(['id', 'image', 'tags', 'width', 'height', 'character', 'path'])
     Path=folder+"/"+Character
     with open(filename, 'a', newline='') as f:
         writer = csv.writer(f)
@@ -55,6 +56,13 @@ def descargar(url, images, character, folder):
         
         # Descargar cada imagen en la página actual
         for item in data:
+            # Obtener la extensión del archivo de imagen
+            image_extension = os.path.splitext(item["image"])[1].lower()
+            
+            # Verificar si la extensión es válida (.jpg o .png)
+            if image_extension not in ['.jpg', '.png']:
+                continue
+            
             # Guardar la respuesta JSON en un archivo
             with open(f'safebooru_{image_count}.json', 'w') as f:
                 json.dump(item, f)
@@ -83,7 +91,7 @@ def descargar(url, images, character, folder):
 def RedNeuronal(csv_file):
     # Cargar el archivo CSV
     df = pd.read_csv(csv_file)
-    image_paths = df['image_path'].tolist()
+    image_paths = df['Path'].tolist()
 
     # Dividir el conjunto de datos en entrenamiento, prueba y validación
     train_ratio = 0.6
@@ -217,34 +225,42 @@ def randomize_csv():
     print("Archivo randomizado generado con éxito.")
 
 
-def main():
 
+def descargar_wrapper(url, images, character, folder):
+    descargar(url, images, character, folder)
+
+def main():
     # Definir el número de imágenes a descargar para cada personaje
     image_count = 5000
 
     # Definir la lista de personajes
     # Lista de personajes y sus respectivos enlaces
     characters = {
-        'Hakurei Reimu': 'https://safebooru.org/index.php?page=post&s=list&tags=hakurei_reimu+1girl+touhou',
-        'Kirisame Marisa': 'https://safebooru.org/index.php?page=post&s=list&tags=kirisame_marisa+1girl+touhou',
-        'Cirno': 'https://safebooru.org/index.php?page=post&s=list&tags=cirno+1girl+touhou',
-        'Komeiji Koishi': 'https://safebooru.org/index.php?page=post&s=list&tags=komeiji_koishi+1girl+touhou',
-        'Komeiji Satori': 'https://safebooru.org/index.php?page=post&s=list&tags=komeiji_satori+1girl+touhou',
-        'Yakumo Yukari': 'https://safebooru.org/index.php?page=post&s=list&tags=yakumo_yukari+1girl+touhou',
-        'Shameimaru Aya': 'https://safebooru.org/index.php?page=post&s=list&tags=shameimaru_aya+1girl+touhou',
-        'Fujiwara No Mokou': 'https://safebooru.org/index.php?page=post&s=list&tags=fujiwara_no_mokou+1girl+touhou',
-        'Houraisan Kaguya': 'https://safebooru.org/index.php?page=post&s=list&tags=houraisan_kaguya+1girl+touhou',
-        'Rumia': 'https://safebooru.org/index.php?page=post&s=list&tags=rumia+1girl+touhou'
+        'Hakurei Reimu': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=hakurei_reimu+1girl+touhou',
+        'Kirisame Marisa': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=kirisame_marisa+1girl+touhou',
+        'Cirno': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=cirno+1girl+touhou',
+        'Komeiji Koishi': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=komeiji_koishi+1girl+touhou',
+        'Komeiji Satori': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=komeiji_satori+1girl+touhou',
+        'Yakumo Yukari': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=yakumo_yukari+1girl+touhou',
+        'Shameimaru Aya': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=shameimaru_aya+1girl+touhou',
+        'Fujiwara No Mokou': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=fujiwara_no_mokou+1girl+touhou',
+        'Houraisan Kaguya': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=houraisan_kaguya+1girl+touhou',
+        'Rumia': 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=rumia+1girl+touhou'
     }
 
     # Crear una carpeta para cada personaje y descargar las imágenes
-    for character in characters:
-        print(f'Downloading images of {character}...')
-        character_folder = character.lower().replace(' ', '_')
-        os.makedirs(character_folder, exist_ok=True)
-        descargar(characters[character], image_count, character, character_folder)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for character in characters:
+            print(f'Downloading images of {character}...')
+            character_folder = character.lower().replace(' ', '_')
+            os.makedirs(character_folder, exist_ok=True)
+            future = executor.submit(descargar_wrapper, characters[character], image_count, character, character_folder)
+            futures.append(future)
 
-    RedNeuronal("archivo.csv")
+        # Esperar a que todas las tareas se completen
+        concurrent.futures.wait(futures)
 
 if __name__ == '__main__':
     main()
+
